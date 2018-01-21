0002f3e2705f55483d9b384fb188d1442f27438f
cosmos860216 <cosmos19970216@gmail.com>
2018-01-21 16:35:06 +0800

    lab

---
+++ lab1/0416305/MeshExample/vec3.h
@@ -0,0 +1,37 @@
+#pragma once
+class Vec3		// vList, nList, tList ªº structure
+{
+public:
+	float ptr[3];
+	Vec3()
+	{
+		for (int i = 0; i < 3; ++i)
+		{
+			ptr[i] = 0;
+		}
+	}
+	Vec3(float *v)
+	{
+		for (size_t i = 0; i<3; i++)
+			ptr[i] = v[i];
+	}
+	inline float& operator[](size_t index)
+	{
+		return ptr[index];
+	}
+	Vec3 operator +=(Vec3 a)
+	{
+		ptr[0] += a[0];
+		ptr[1] += a[1];
+		ptr[2] += a[2];
+		return *this;
+	}
+	Vec3 operator -=(Vec3 a)
+	{
+		ptr[0] -= a[0];
+		ptr[1] -= a[1];
+		ptr[2] -= a[2];
+		return *this;
+	}
+
+};
